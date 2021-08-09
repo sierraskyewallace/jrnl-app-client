0952828e1116d7a8101186ddcc510202c84c1502
sierraskyewallace <sskwallace@gmail.com>
2021-08-09 16:46:31 -0700

    users json in console

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const endPoint = "http://localhost:3000/api/v1/users"
+
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("loaded");
+    fetch(endPoint)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+            }
+        )
 });
