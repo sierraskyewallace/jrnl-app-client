e5927a2d56621b1abbe8ed6c60cd530ccb2bd3a0
sierraskyewallace <sskwallace@gmail.com>
2021-08-25 13:04:42 -0700

    Added entry forms but need to branch out and begin auth/users routes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    getEntries();
-});
-;
+    getEntries()
+    const createEntryForm = document.querySelector('#journal-entry-form-container');
+    createEntryForm.addEventListener("submit", (event) => {
+        createFormHandler(event)
+    });
 
     //function that gets the entries from the api and renders them
     function getEntries() {
@@ -24,8 +26,37 @@ document.addEventListener('DOMContentLoaded', () => {
             });
     }
 
-    //function that adds a new entry to the api
-   
+    function createFormHandler(event) {
+        event.preventDefault();
+        const name = document.querySelector('#input-name').value;
+        const content = document.querySelector('#input-content').value;
+        const newEntry = {
+            name: name,
+            content: content,
+        };
+        fetch("http://localhost:3000/api/v1/journal_entries", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newEntry)
+        })
+            .then(response => response.json())
+            .then(newEntry => {
+                console.log(newEntry);
+                const journalEntryMarkup = `
+            <div data-id="${newEntry.id}">
+            <h3>${newEntry.attributes.name}</h3>
+            // add the date here
+            <p>${newEntry.attributes.content}</p>
+            </div>
+            <br><br>`;
+                document.querySelector('#journal-entries-container').innerHTML += journalEntryMarkup;
+            });
+            
 
-    // function that signs a user in by username then renders their entries
+
     
+
+        // function that signs a user in by username then renders their entries
+}});
