f83da36bd73388d5ed51ca4ca43b5d003fcfcd27
sierraskyewallace <sskwallace@gmail.com>
2021-09-14 13:51:13 -0700

    removed user.js, unecassary

--- journal_entry.js
+++ journal_entry.js
@@ -17,17 +17,29 @@ renderEntry() {
         <div class="accordion" id="accordionExample">
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+      <button
+        class="accordion-button"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-mdb-target="#collapseOne"
+        aria-expanded="true"
+        aria-controls="collapseOne"
+      >
         ${this.name}
       </button>
     </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+    <div
+      id="collapseOne"
+      class="accordion-collapse collapse show"
+      aria-labelledby="headingOne"
+      data-mdb-parent="#accordionExample"
+    >
       <div class="accordion-body">
-        <p>${this.content}</p>
+        <p> ${this.content} </p>
       </div>
+    </div>
   </div>
-</div>
-</div>
+
     `;
     }
 }
--- user.js
+++
@@ -1,8 +0,0 @@
-
-class User {
-    constructor(id, username, password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
-}
